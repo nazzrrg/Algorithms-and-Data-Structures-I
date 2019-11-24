9b53769b65e2ceebc0b9e1341bfe78b6047554e1
nazzrrg <nazarov.egor2010@yandex.ru>
2019-11-24 22:20:24 +0300

    Work in progress on Task 1 in Lab 6

--- Algorithms and Data Structures/lab6/Task 1/main.cpp
+++ Algorithms and Data Structures/lab6/Task 1/main.cpp
@@ -15,26 +15,117 @@ public:
     void insert(long long key) {
         auto z = new Node;
         z->key = key;
+        Node* ptr = root;
 
-        while (root != nullptr) {
-            if (z->key > root->key) {
-                if (root->right != nullptr) {
-                    root = root->right;
+        while (ptr != nullptr) {
+            if (z->key > ptr->key) {
+                if (ptr->right != nullptr) {
+                    ptr = ptr->right;
                 } else {
-                    z->parent = root;
-                    root->right = z;
+                    z->parent = ptr;
+                    ptr->right = z;
                     break;
                 }
-            } else if (z->key < root->key) {
-                if (root->left != nullptr) {
-                    root = root->left;
+            } else if (z->key < ptr->key) {
+                if (ptr->left != nullptr) {
+                    ptr = ptr->left;
                 } else {
-                    z->parent = root;
-                    root->left = z;
+                    z->parent = ptr;
+                    ptr->left = z;
                     break;
                 }
             }
         }
+    }
+
+    Node* find(long long key) {
+        Node* ptr = root;
+
+        while (ptr != nullptr) {
+            if (key > ptr->key) {
+                if (ptr->right != nullptr) {
+                    ptr = ptr->right;
+                } else {
+                    return nullptr;
+                }
+            } else if (key < ptr->key) {
+                if (ptr->left != nullptr) {
+                    ptr = ptr->left;
+                } else {
+                    return nullptr;
+                }
+            } else if (key == ptr->key){
+                return ptr;
+            }
+        }
+
+        return nullptr;
+    }
+
+    Node* next(long long key) {
+        Node* current = root;
+        Node* successor = nullptr;
+
+        while (current != nullptr) {
+            if (current->key > key) {
+                successor = current;
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return successor;
+    }
+
+    void remove(long long key) {
+        Node* ptr = find(key);
+
+        if (ptr->left == nullptr && ptr->right == nullptr) {
+            if (ptr->parent->left == ptr) {
+                ptr->parent->left = nullptr;
+            }
+            if (ptr->parent->right == ptr) {
+                ptr->parent->right = nullptr;
+            }
+            delete ptr;
+            return;
+        }
+
+        if (ptr->left == nullptr || ptr->right == nullptr) {
+            if (ptr->left == nullptr) {
+                if (ptr->parent->left == ptr) {
+                    ptr->parent->left = ptr->right;
+                } else {
+                    ptr->parent->right = ptr->right;
+                }
+                ptr->right->parent = ptr->parent;
+            } else {
+                if (ptr->parent->left == ptr) {
+                    ptr->parent->left = ptr->left;
+                } else {
+                    ptr->parent->right = ptr->left;
+                }
+                ptr->left->parent = ptr->parent;
+            }
+            delete ptr;
+            return;
+        }
+
+        Node* successor = next(key);
+        ptr->key = successor->key;
+        if (successor->parent->left == successor) {
+            successor->parent->left = successor->right;
+            if (successor->right != nullptr) {
+                successor->right->parent = successor->parent;
+            }
+        } else {
+            successor->parent->right = successor->right;
+            if (successor->right != nullptr) {
+                successor->right->parent = successor->parent;
+            }
+        }
+
+        delete successor;
     }
 };
 
