8de9a83c04e625b091d4f12b0c76364d35283596
nazzrrg <nazarov.egor2010@yandex.ru>
2019-12-02 16:46:25 +0300

    Lab 6 Task 2 All tests passed

--- Algorithms and Data Structures/lab6/Task 2/main.cpp
+++ Algorithms and Data Structures/lab6/Task 2/main.cpp
@@ -3,24 +3,14 @@
 
 using namespace std;
 
-bool dfs(vector<vector<int>>& v, int i, int lb, int rb, bool right) {
+bool dfs(vector<vector<int>>& v, int i, int lb, int rb) {
     if (i < 0) return true;
 
-    if (right) {
-        if (v[i][0] >= rb) {
-            rb = v[i][0];
-            return dfs(v, v[i][1], lb, rb, false) && dfs(v, v[i][2], lb, rb, true);
-        } else {
-            return false;
-        }
-    } else {
-        if (v[i][0] < lb) {
-            lb = v[i][0];
-            return dfs(v, v[i][1], lb, rb, false) && dfs(v, v[i][2], lb, rb, true);
-        } else {
-            return false;
-        }
-    }
+//    cout << "DFS from " << v[i][0] << " with lb = " << lb << " rb = " << rb << endl;
+
+    if (v[i][0] <= lb || v[i][0] >= rb) return false;
+
+    return (dfs(v, v[i][1], lb, v[i][0]) && dfs(v, v[i][2], v[i][0], rb));
 }
 
 int main() {
@@ -33,7 +23,7 @@ int main() {
     cin >> n;
 
     if (n == 0) {
-        cout << 0;
+        cout << "YES";
         return 0;
     }
     v.resize(n);
@@ -46,7 +36,7 @@ int main() {
         v[i].push_back(r-1);
     }
 
-    if (dfs(v, 0, v[0][0], v[0][0], true)) {
+    if (dfs(v, 0, INT32_MIN, INT32_MAX)) {
         cout << "YES";
     } else {
         cout << "NO";
